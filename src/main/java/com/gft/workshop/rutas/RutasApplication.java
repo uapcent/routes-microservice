5ab85c88ab275f8a54edaeee9a8828de3aab2b7c
Pau <uapcent@gmail.com>
2024-07-15 12:25:52 +0200

    second commit

---
+++ src/main/java/com/gft/workshop/rutas/RutasApplication.java
@@ -0,0 +1,13 @@
+package com.gft.workshop.rutas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RutasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RutasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gft/workshop/rutas/RutasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.workshop.rutas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RutasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
